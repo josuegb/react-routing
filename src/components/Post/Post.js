7eaf62f65b5448361c101bc76682703a8b11b71d
josuegb15 <josueg@thehangar.cr>
2018-06-05 14:00:28 -0600

    The "withRouter" HOC & Route Props

--- src/components/Post/Post.js
+++ src/components/Post/Post.js
@@ -1,5 +1,7 @@
 import React from 'react';
 
+// import { withRouter } from 'react-router-dom';
+
 import './Post.css';
 
 const post = (props) => (
@@ -12,3 +14,19 @@ const post = (props) => (
 );
 
 export default post;
+
+// const post = (props) => {
+
+//   console.log(props)
+
+//   return (
+//     <article className="Post" onClick={props.clicked}>
+//       <h1>{props.title}</h1>
+//       <div className="Info">
+//         <div className="Author">{props.author}</div>
+//       </div>
+//     </article>
+//   );
+// };
+
+// export default withRouter(post);
